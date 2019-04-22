f96011a2a60970aa8316c2fb90b96c2fbb6af833
chuxu <s17chucc@163.com>
2019-04-22 17:05:49 +0800

    布局组件完成

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,166 @@
+import Taro, { Component } from '@tarojs/taro';
+import { View } from '@tarojs/components';
+import { SIZE } from '../utils/model';
+
+type flexSelectionType = {
+  justify?: 'start' | 'end' | 'center' | 'between' | 'around';
+  align?: 'start' | 'center' | 'end';
+  warp?: boolean;
+};
+
+type gridSelectionType = {
+  col: number;
+  mode?: 'square';
+};
+
+type direction =
+  | 'around'
+  | 'horizontal'
+  | 'vertical'
+  | 'left'
+  | 'right'
+  | 'top'
+  | 'bottom';
+type distance = 'xsmall' | 'small' | 'normal' | 'large' | 'xlarge';
+
+type baseSelectionType = {
+  float?: 'left' | 'right';
+  padding: distance;
+  paddingDirection?: direction;
+  margin: distance;
+  marginDirection?: direction;
+};
+interface IProps {
+  type?: 'flex' | 'grid' | 'normal';
+  flexSelection?: flexSelectionType;
+  gridSelection?: gridSelectionType;
+  baseSelection?: baseSelectionType;
+}
+
+/**
+ * flex 问题
+ * 浮动比例
+ * flex-sub==>1, flex-twice==>2, flex-treble==>3
+ * 基础比例
+ * basis-xs, basis-df, basis-lg, basis-xl
+ */
+
+
+interface IState {}
+
+export default class ClLayout extends Component<IProps, IState> {
+  static options = {
+    addGlobalClass: true
+  };
+  static defaultProps = {
+    type: 'normal',
+    flexSelection: {
+      justify: 'start',
+      align: 'start',
+      warp: false
+    }
+  } as IProps;
+  render() {
+    const warpClassName =
+      this.props.flexSelection && this.props.flexSelection.warp
+        ? 'flex-wrap'
+        : '';
+    const justifyClassName =
+      this.props.flexSelection && this.props.flexSelection.justify
+        ? `justify-${this.props.flexSelection.justify}`
+        : '';
+    const alignClassName =
+      this.props.flexSelection && this.props.flexSelection.align
+        ? `align-${this.props.flexSelection.align}`
+        : '';
+    const flexComponent = (
+      <View
+        className={`flex ${warpClassName} ${justifyClassName} ${alignClassName}`}
+      >
+        {this.props.children}
+      </View>
+    );
+
+    const colClassName =
+      this.props.gridSelection && `col-${this.props.gridSelection.col}`;
+    const squareClassName =
+      this.props.gridSelection && this.props.gridSelection.mode === 'square'
+        ? 'grid-square'
+        : '';
+    const gridComponent = (
+      <View className={`grid ${colClassName} ${squareClassName}`}>
+        {this.props.children}
+      </View>
+    );
+    const floatClassName = () => {
+      if (this.props.baseSelection && this.props.baseSelection.float === 'left')
+        return 'fl';
+      if (
+        this.props.baseSelection &&
+        this.props.baseSelection.float === 'right'
+      )
+        return 'fr';
+      return '';
+    };
+    const dealDirection = (direction): string => {
+      let dr: string;
+      switch (direction) {
+        case 'around': {
+          dr = '';
+          break;
+        }
+        case 'horizontal': {
+          dr = '-lr';
+          break;
+        }
+        case 'vertical': {
+          dr = '-tb';
+        }
+        default: {
+          dr = direction ? `-${direction}` : '';
+        }
+      }
+      return dr;
+    };
+    const dealSize = (size: any) => {
+      if (!size || size === 'normal') return '';
+      else return `-${SIZE[size]}`;
+    };
+    const paddingClassName = () => {
+      const paddingDirection =
+        this.props.baseSelection && this.props.baseSelection.paddingDirection;
+      const size = this.props.baseSelection && this.props.baseSelection.padding;
+
+      return `${'padding' + dealDirection(paddingDirection) + dealSize(size)}`;
+    };
+    const marginClassName = () => {
+      const marginDirection =
+        this.props.baseSelection && this.props.baseSelection.marginDirection;
+      const size = this.props.baseSelection && this.props.baseSelection.margin;
+
+      return `${'margin' + dealDirection(marginDirection) + dealSize(size)}`;
+    };
+    const normalComponent = (
+      <View
+        className={`${floatClassName()} ${
+          this.props.baseSelection && this.props.baseSelection.padding
+            ? paddingClassName()
+            : ''
+        } ${
+          this.props.baseSelection && this.props.baseSelection.margin
+            ? marginClassName()
+            : ''
+        }`}
+      >
+        {this.props.children}
+      </View>
+    );
+    return this.props.type === 'normal'
+      ? normalComponent
+      : this.props.type === 'grid'
+      ? gridComponent
+      : this.props.type === 'flex'
+      ? flexComponent
+      : normalComponent;
+  }
+}
