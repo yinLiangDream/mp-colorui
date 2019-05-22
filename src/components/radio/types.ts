5ed709674909b7b0b55af0432331eaacaa58b641
chuxu <s17chucc@163.com>
2019-05-22 17:37:33 +0800

    radio 注释增加

--- src/components/radio/types.ts
+++ src/components/radio/types.ts
@@ -5,7 +5,13 @@ export type TShape = 'normal' | 'radio';
 export type TTitle = string;
 export type TColor = bgColorType;
 export type TList = {
+  /**
+   * 选项框的显示标题
+   */
   key?: string;
+  /**
+   * 选中的时的传递的值
+   */
   value?: string;
 }[];
 export type TRadioDirection = 'vertical' | 'horizontal';
@@ -13,12 +19,50 @@ export type TRadioDirection = 'vertical' | 'horizontal';
 export type TOnChange = (value: any) => void;
 
 export interface IProps {
+  /**
+   * 单选框类型
+   *
+   * 默认值 `normal`
+   *
+   * 可选类型 `normal`, `form`
+   */
   type?: TType;
+  /**
+   * 单选框方向
+   *
+   * 默认值 `vertical`
+   *
+   * 可选类型 `vertical`, `horizontal`
+   */
   direction?: TRadioDirection;
+  /**
+   * 单选框形状
+   *
+   * 默认值 `normal`
+   *
+   * 可选类型 `normal`, `radio`
+   */
   shape?: TShape;
+  /**
+   *  type 为 form 下的标题
+   */
   title?: TTitle;
+  /**
+   * 选中颜色，可选类型请查看 默认基本色 可选类型
+   */
   color?: TColor;
+  /**
+   * 默认选中的 value
+   */
   checkedValue?: string;
+  /**
+   * 单选框组
+   *
+   * 每一项可设置以下参数 `key`, `value`
+   */
   radioGroup?: TList;
+  /**
+   * 选项发生改变事件
+   */
   onChange?: TOnChange;
 }
