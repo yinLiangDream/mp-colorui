8dec4610627d9271b57c2f0722a0fe46ecd6783c
chuxu <s17chucc@163.com>
2019-05-22 16:10:38 +0800

    drawer 注释增加

--- src/components/drawer/types.ts
+++ src/components/drawer/types.ts
@@ -5,7 +5,21 @@ export type TDirection = 'left' | 'right' | 'bottom';
 export type TChildren = ReactNode;
 
 export interface IProps {
+  /**
+   * 是否显示
+   *
+   * 默认值 `false`
+   *
+   * 可选类型 `true`, `false`
+   */
   show?: boolean;
+  /**
+   * 抽屉方向
+   *
+   * 默认值 `bottom`
+   *
+   * 可选类型 `left`, `right`, `bottom`
+   */
   direction?: TDirection;
   children?: TChildren;
 }
