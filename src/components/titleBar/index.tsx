c2a9669c296621d2b2296e737fe1af6e4414e8ed
chuxu <s17chucc@163.com>
2019-05-23 11:12:26 +0800

    titleBar 注释增加

--- src/components/titleBar/index.tsx
+++ src/components/titleBar/index.tsx
@@ -1,14 +1,9 @@
-import { Text, View } from "@tarojs/components";
+import { Text, View } from '@tarojs/components';
 import Taro, { Component } from '@tarojs/taro';
-import { BG_COLOR_LIST } from "../utils/model";
+import { BG_COLOR_LIST } from '../utils/model';
 import { IProps } from './types';
 
-
-
-
-interface IState {
-
-}
+interface IState {}
 
 export default class ClTitleBar extends Component<IProps, IState> {
   static options = {
@@ -21,42 +16,66 @@ export default class ClTitleBar extends Component<IProps, IState> {
     type: 'border-title',
     borderLong: 20,
     borderColor: 'green',
-    icon: "title",
-    iconColor: "green",
-    text: '',
+    icon: 'title',
+    iconColor: 'green',
+    title: '',
     subTitle: '',
-    subTitleColor: "gray"
+    subTitleColor: 'gray'
   };
 
   render() {
-    const textColorClassName = this.props.textColor ? `text-${this.props.textColor}` : ``;
-    const borderColorClassName = this.props.borderColor ? BG_COLOR_LIST[this.props.borderColor] : 'bg-green';
-    const barColorClassName = this.props.barColor ? BG_COLOR_LIST[this.props.barColor] : `bg-white`;
+    const textColorClassName = this.props.textColor
+      ? `text-${this.props.textColor}`
+      : ``;
+    const borderColorClassName = this.props.borderColor
+      ? BG_COLOR_LIST[this.props.borderColor]
+      : 'bg-green';
+    const barColorClassName = this.props.barColor
+      ? BG_COLOR_LIST[this.props.barColor]
+      : `bg-white`;
     const iconClassName = this.props.icon ? `cuIcon-${this.props.icon}` : ``;
-    const iconColorClassName = this.props.iconColor ? `text-${this.props.iconColor}` : ``;
-    const borderComponent = <View className='action border-title'>
-      <Text className={`${textColorClassName} text-bold text-xl`}>{this.props.text}</Text>
-      <Text className={`${borderColorClassName}`} style={{'width': `${this.props.borderLong}rpx`}}/>
-    </View>;
-    const subComponent = <View className='action sub-title'>
-      <Text className={`text-xl text-bold ${textColorClassName}`}>{this.props.text}</Text>
-      {
-        this.props.subTitle ? <Text className={`text-${this.props.subTitleColor}`}>{this.props.subTitle}</Text> :
+    const iconColorClassName = this.props.iconColor
+      ? `text-${this.props.iconColor}`
+      : ``;
+    const borderComponent = (
+      <View className='action border-title'>
+        <Text className={`${textColorClassName} text-bold text-xl`}>
+          {this.props.title}
+        </Text>
+        <Text
+          className={`${borderColorClassName}`}
+          style={{ width: `${this.props.borderLong}rpx` }}
+        />
+      </View>
+    );
+    const subComponent = (
+      <View className='action sub-title'>
+        <Text className={`text-xl text-bold ${textColorClassName}`}>
+          {this.props.title}
+        </Text>
+        {this.props.subTitle ? (
+          <Text className={`text-${this.props.subTitleColor}`}>
+            {this.props.subTitle}
+          </Text>
+        ) : (
           <Text className={`${borderColorClassName}`}>{}</Text>
-      }
-
-    </View>;
-    const iconComponent =
+        )}
+      </View>
+    );
+    const iconComponent = (
       <View className='action'>
-        <Text className={`${iconClassName} ${iconColorClassName}`}/>
-        <Text className={`text-xl text-bold ${textColorClassName}`}>{this.props.text}</Text>
-      </View>;
+        <Text className={`${iconClassName} ${iconColorClassName}`} />
+        <Text className={`text-xl text-bold ${textColorClassName}`}>
+          {this.props.title}
+        </Text>
+      </View>
+    );
     return (
       <View className={`${barColorClassName} cu-bar`}>
         {this.props.type === 'border-title' ? borderComponent : ''}
         {this.props.type === 'sub-title' ? subComponent : ''}
         {this.props.type === 'icon' ? iconComponent : ''}
       </View>
-    )
+    );
   }
 }
--- src/components/titleBar/types.ts
+++ src/components/titleBar/types.ts
@@ -1,14 +1,55 @@
-import { bgColorType, bgColorMoreType, lightBgColorType, iconType } from "../utils/types";
+import {
+  bgColorType,
+  bgColorMoreType,
+  lightBgColorType,
+  iconType
+} from '../utils/types';
 
 export interface IProps {
+  /**
+   * 标题栏背景色，可选类型请查看 默认背景色 可选类型
+   */
   barColor?: bgColorType | bgColorMoreType | lightBgColorType;
+  /**
+   * 文字颜色，可选类型请查看 默认基本色 可选类型
+   */
   textColor?: bgColorType;
+  /**
+   * 子标题类型
+   *
+   * 默认值 `border-title`
+   *
+   * 可选类型 `border-title`, `sub-title`, `icon`
+   */
   type?: 'border-title' | 'sub-title' | 'icon';
+  /**
+   * 仅 type 为 sub-title时设置，子标题
+   */
   subTitle?: string;
+  /**
+   * 子标题颜色，可选类型请查看 默认基本色 可选类型
+   */
   subTitleColor?: bgColorType;
+  /**
+   * 仅 type 为 border-title 时设置，子边线，可选类型请查看 默认背景色 可选类型
+   */
   borderColor?: bgColorType | bgColorMoreType | lightBgColorType;
+  /**
+   * 子边线长度
+   *
+   * 默认值 `20`
+   */
   borderLong?: number;
+  /**
+   * 仅 type 为 icon 时设置，图标，可选类型请查看 icon 可选类型
+   */
   icon?: iconType;
+  /**
+   * 图标颜色，可选类型请查看 默认基本色 可选类型
+   */
   iconColor?: bgColorType;
-  text?: string;
+  /**
+   * 标题
+   */
+  title?: string;
 }
