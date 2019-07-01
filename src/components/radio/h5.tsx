b0264470612e0657179c7adfa82dc2ee3353010c
LY <s17chucc@163.com>
2019-07-01 10:22:53 +0800

    修复 H5 Radio 样式问题

--- src/components/radio/h5.tsx
+++ src/components/radio/h5.tsx
@@ -1,55 +1,54 @@
-import {Radio, RadioGroup, Text, View} from '@tarojs/components';
-import Taro from '@tarojs/taro';
-import H5Radio from './h5';
-import {IProps} from '../../../@types/radio';
+import Taro, {useEffect, useState} from '@tarojs/taro'
+import {Text, View} from "@tarojs/components";
+
+import { IProps } from '../../../@types/radio';
 
 
-export default function ClRadio(props: IProps) {
+export default function RadioH5(props: IProps) {
   const type = props.type === 'form' ? 'form' : 'normal';
   const shapeClassName = props.shape === 'radio' ? 'radio' : 'normal';
   const title = props.title ? props.title : '';
   const colorClassName = props.color ? props.color : 'green';
   const directionClassName = props.direction === 'horizontal' ? 'flex' : '';
   const list = props.radioGroup ? props.radioGroup : [];
+  const [activValue, setActiveValue] = useState(props.checkedValue);
 
-  const changeRadio = e => {
-    props.onChange && props.onChange(e.detail.value);
+  const clickRadio = (name, index) => {
+    setActiveValue(name);
   };
+
+  useEffect(() => {
+    props.onChange && props.onChange(activValue)
+  }, [activValue]);
+
   const radioComponent = list.map((item, index) => (
     <View className='padding-xs' key={item.value}>
       <Text className='padding-right-sm'>{item.key}</Text>
-      <Radio
-        className={`${colorClassName} ${shapeClassName}`}
-        checked={item.value === props.checkedValue}
-        value={item.value || ''}
-      />
+      <View onClick={() => {clickRadio(item.value, index)}} className={`h5-radio ${colorClassName} ${shapeClassName} ${activValue === item.value ? 'checked' : ''}`}>
+        <View className='h5-radio-wrapper'>
+          <View className='h5-radio-input h5-radio-input-checked'></View>
+        </View>
+      </View>
     </View>
   ));
+
   const formRadioComponent = (
     <View className='cu-form-group'>
       <View className='title'>{title}</View>
       <View className={directionClassName}>{radioComponent}</View>
     </View>
   );
-
-  const mpRadio =
-    <RadioGroup
-      onChange={changeRadio}
-      className={`${props.type === 'form' ? 'block' : ''}`}
-    >
+  return (
+    <View className='h5-radio-group'>
       {type === 'form' ? (
         formRadioComponent
       ) : (
         <View className={directionClassName}>{radioComponent}</View>
       )}
-    </RadioGroup>;
-  const RadioComponent = Taro.getEnv() === Taro.ENV_TYPE.WEB ? <H5Radio {...props} /> : mpRadio;
-
-  return (
-    RadioComponent
-  );
+    </View>
+  )
 }
 
-ClRadio.options = {
+RadioH5.options = {
   addGlobalClass: true
-};
+}
