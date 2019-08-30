4db34fc74c8b6111fd1020d2db4784b0b9294972
chuxu <s17chucc@163.com>
2019-08-30 15:28:33 +0800

    BUG 修复 Tip 组件 H5 下显示位置异常问题

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -48,6 +48,22 @@ export default class Index extends Taro.Component {
     const {filterResult, result, showLoading} = this.state
     return (
       <View>
+        <ClLayout padding='normal' paddingDirection='around'>
+          <ClFlex justify='between'>
+            <ClTip message='我是上方提示' direction='top'>
+              <ClButton text='上方' shape='round' />
+            </ClTip>
+            <ClTip message='我是右方提示' direction='right'>
+              <ClButton text='右方' shape='round' />
+            </ClTip>
+            <ClTip message='我是左方提示' direction='left'>
+              <ClButton text='左方' shape='round' />
+            </ClTip>
+            <ClTip message='我是下方提示' direction='bottom'>
+              <ClButton text='下方' shape='round' />
+            </ClTip>
+          </ClFlex>
+        </ClLayout>
         <ClSearchBar
           shape='radius'
           bgColor='gradualBlue'
