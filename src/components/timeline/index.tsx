1f069df556824aad98ff9306ab8c3c8b010c24fb
chuxu <s17chucc@163.com>
2019-09-06 13:51:57 +0800

    修复 Timeline 组件因为 key 值编译时报 warning 问题

--- src/components/timeline/index.tsx
+++ src/components/timeline/index.tsx
@@ -3,24 +3,22 @@ import Taro, { pxTransform } from '@tarojs/taro';
 import { BG_COLOR_LIST, TEXT_COLOR_LIST } from '../utils/model';
 import { IProps } from '../../../@types/timeline';
 import ClFlex from "../flex";
+import { generateId } from '../utils'
 
 export default function ClTimeline(props: IProps) {
   const times = props.times || [];
   const iconColorClassName = color => (color ? TEXT_COLOR_LIST[color] : '');
   const iconClassName = icon => (icon ? `cuIcon-${icon}` : '');
   const bgColorClassName = color => (color ? BG_COLOR_LIST[color] : '');
-  const renderNode = (item, index) => {
-    return (
-      <View className='cu-time' key={index}>{item.node}</View>
-    )
-  }
-  const renderNormal = (item, index) => {
-    return (
+  const items = times.map((item) => (
+    item.node ?
+      <View className='cu-time' key={generateId()}>{item.node}</View>
+      :
       <View
         className={`cu-item ${iconColorClassName(item.iconColor)} ${iconClassName(
           item.icon
         )}`}
-        key={index}
+        key={generateId()}
       >
         <View className={`${bgColorClassName(item.bgColor)} content`}>
           <ClFlex justify='between' align='end'>
@@ -34,10 +32,6 @@ export default function ClTimeline(props: IProps) {
           }
         </View>
       </View>
-    )
-  }
-  const items = times.map((item, index) => (
-    item.node ? renderNode(item, index) : renderNormal(item, index)
   ));
   return <View className='cu-timeline'>{items}</View>;
 }
