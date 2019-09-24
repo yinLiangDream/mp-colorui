0b5ff48d8bdd172d28f634799ae3a09385aff58b
chuxu <s17chucc@163.com>
2019-09-24 16:10:27 +0800

    BUG 支付宝小程序 Radio 适配

--- src/components/checkbox/h5/index.tsx
+++ src/components/checkbox/h5/index.tsx
@@ -1,85 +1,70 @@
 import { View, Text } from '@tarojs/components';
-import Taro, { Component, useState, pxTransform } from '@tarojs/taro';
+import Taro, { pxTransform, useMemo, useState } from '@tarojs/taro'
 import { IProps } from '../../../../@types/checkbox';
 
-interface IState {
-  list: any[] | undefined;
-}
-
-export default class ClCheckbox_h5 extends Component<IProps, IState> {
-  static options = {
-    addGlobalClass: true
-  };
-  static defaultProps: IProps = {
-    color: 'green',
-    type: 'normal',
-    shape: 'normal',
-    direction: 'vertical',
-    checkboxGroup: [],
-    title: ''
-  };
-  state: IState = {
-    list: this.props.checkboxGroup || []
-  };
-  change() {
-    const checkboxGroup = this.state.list || [];
-    const arr = checkboxGroup
+export default function ClCheckboxH5 (props: IProps) {
+  const [list, setList] = useState(props.checkboxGroup || [])
+  useMemo(() => {
+    setList(props.checkboxGroup || [])
+  }, [props.checkboxGroup])
+  const change = () => {
+    const arr = list
       .filter(item => item.checked)
       .map(item => item.value || '');
-    this.props.onChange && this.props.onChange(arr);
+    props.onChange && props.onChange(arr)
   }
-  renderCheckbox(classname = '', value = '', checked = false) {
-    const [check, setCheck] = useState(checked);
-    return (
+
+  const colorClassName = props.color || 'green'
+  const type = props.type === 'form' ? 'form' : ''
+  const shapeClassName = props.shape === 'round' ? 'round' : ''
+  const directionClassName =
+    props.direction === 'horizontal' ? 'flex' : ''
+  const title = props.title
+  const checkboxComponent = list.map(item => (
+    <View className='padding-xs' key={item.value}>
+      <Text
+        className='padding-right-sm'
+        style={{
+          lineHeight: `${pxTransform(46)}`,
+          display: 'inline-block',
+          verticalAlign: 'text-bottom'
+        }}
+      >
+        {item.key}
+      </Text>
       <View
-        className={`h5-checkbox-input ${classname} ${check ? 'checked' : ''}`}
+        className={`h5-checkbox-input ${shapeClassName} ${item.checked ? 'checked' : ''}`}
         onClick={() => {
-          setCheck(!check);
-          const { list } = this.state
-          const checkItem = list && list.find(item => item.value === value);
-          if (checkItem) checkItem.checked = !check;
-          this.change();
+          console.log(item.value)
+          const checkItem = list && list.find(has => has.value === item.value)
+          if (checkItem) checkItem.checked = !item.checked
+          setList(list)
+          change()
         }}
       />
-    );
-  }
-  render() {
-    const colorClassName = this.props.color || 'green';
-    const type = this.props.type === 'form' ? 'form' : '';
-    const shapeClassName = this.props.shape === 'round' ? 'round' : '';
-    const directionClassName =
-      this.props.direction === 'horizontal' ? 'flex' : '';
-    const listCheckboxGroup = this.props.checkboxGroup ? this.props.checkboxGroup : [];
-    const title = this.props.title;
-    const checkboxComponent = listCheckboxGroup.map(item => (
-      <View className='padding-xs' key={item.value}>
-        <Text
-          className='padding-right-sm'
-          style={{
-            lineHeight: `${pxTransform(46)}`,
-            display: 'inline-block',
-            verticalAlign: 'text-bottom'
-          }}
-        >
-          {item.key}
-        </Text>
-        {this.renderCheckbox(
-          `${shapeClassName}`,
-          item.value || '',
-          item.checked
-        )}
-      </View>
-    ));
-    const formComponent = (
-      <View className='cu-form-group'>
-        <View className='title'>{title}</View>
-        <View className={directionClassName}>{checkboxComponent}</View>
-      </View>
-    );
-    return (
-      <View className={`h5-checkbox ${colorClassName}`}>
-        {type === 'form' ? formComponent : checkboxComponent}
-      </View>
-    );
-  }
+    </View>
+  ))
+  const formComponent = (
+    <View className='cu-form-group'>
+      <View className='title'>{title}</View>
+      <View className={directionClassName}>{checkboxComponent}</View>
+    </View>
+  )
+  return (
+    <View className={`h5-checkbox ${colorClassName}`}>
+      {type === 'form' ? formComponent : checkboxComponent}
+    </View>
+  )
 }
+
+ClCheckboxH5.options = {
+  addGlobalClass: true
+}
+ClCheckboxH5.defaultProps = {
+  color: 'green',
+  type: 'normal',
+  shape: 'normal',
+  direction: 'vertical',
+  checkboxGroup: [],
+  title: ''
+} as IProps
--- src/components/checkbox/index.tsx
+++ src/components/checkbox/index.tsx
@@ -1,16 +1,16 @@
-import { Checkbox, CheckboxGroup, Text, View } from '@tarojs/components';
-import Taro from '@tarojs/taro';
-import { IProps } from '../../../@types/checkbox';
-import ClCheckbox_h5 from './h5'
+import { Checkbox, CheckboxGroup, Text, View } from '@tarojs/components'
+import Taro from '@tarojs/taro'
+import { IProps } from '../../../@types/checkbox'
+import ClCheckboxH5 from './h5'
+import { isWeApp } from '../utils'
 
-
-export default function ClCheckbox(props: IProps) {
-  const colorClassName = props.color || 'green';
-  const type = props.type === 'form' ? 'form' : '';
-  const shapeClassName = props.shape === 'round' ? 'round' : '';
-  const directionClassName = props.direction === 'horizontal' ? 'flex' : '';
-  const list = props.checkboxGroup ? props.checkboxGroup : [];
-  const title = props.title;
+export default function ClCheckbox (props: IProps) {
+  const colorClassName = props.color || 'green'
+  const type = props.type === 'form' ? 'form' : ''
+  const shapeClassName = props.shape === 'round' ? 'round' : ''
+  const directionClassName = props.direction === 'horizontal' ? 'flex' : ''
+  const list = props.checkboxGroup ? props.checkboxGroup : []
+  const title = props.title
   const checkboxComponent = list.map((item) => (
     <View className='padding-xs' key={item.value}>
       <Text className='padding-right-sm'>{item.key}</Text>
@@ -20,32 +20,34 @@ export default function ClCheckbox(props: IProps) {
         checked={item.checked}
       />
     </View>
-  ));
+  ))
   const formComponent = (
     <View className='cu-form-group'>
       <View className='title'>{title}</View>
       <View className={directionClassName}>{checkboxComponent}</View>
     </View>
-  );
+  )
   const change = e => {
     props.onChange && props.onChange(e.detail.value)
-  };
-  const env = Taro.getEnv()
-  const web = Taro.ENV_TYPE.WEB;
-  const component = env === web ? <ClCheckbox_h5 {...props} /> :
-  <CheckboxGroup
-  className={`${type === 'form' ? 'block' : ''}`}
-  onChange={change}
->
-  {type === 'form' ? formComponent : <View className={directionClassName}>{checkboxComponent}</View>}
-</CheckboxGroup>
+  }
+  const component = !isWeApp ?
+                    <ClCheckboxH5 {...props} />
+                             :
+                    <CheckboxGroup
+                      className={`${type === 'form' ? 'block' : ''}`}
+                      onChange={change}
+                    >
+                      {type === 'form' ? formComponent : <View
+                        className={directionClassName}
+                      >{checkboxComponent}</View>}
+                    </CheckboxGroup>
   return (
     <View>
       {component}
     </View>
-  );
+  )
 }
 
 ClCheckbox.options = {
   addGlobalClass: true
-};
+}
