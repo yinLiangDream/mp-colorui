e16b3831e230cd12d0b1ed3b42e3b436e6d67dc1
chuxu <s17chucc@163.com>
2019-10-09 10:27:35 +0800

    version 1.update version 2.change homepage address 3.fix #23

--- src/components/timer/timer.test.js
+++ src/components/timer/timer.test.js
@@ -1,48 +1,52 @@
-import Nerv from 'nervjs'
-import { View } from '@tarojs/components'
-import sinon from 'sinon'
+import Nerv from "nervjs";
+import { View } from "@tarojs/components";
+import sinon from "sinon";
 
-import Timer from '../../../.temp/components/timer/timer'
+import Timer from "../../../.temp/components/timer/timer";
 
-describe('Timer test', () => {
-  let scratch
+describe("Timer test", () => {
+  let scratch;
 
   beforeEach(() => {
-    scratch = document.createElement('div')
-    document.body.appendChild(scratch)
-  })
+    scratch = document.createElement("div");
+    document.body.appendChild(scratch);
+  });
 
-  it('Timer test suites', () => {
+  it("Timer test suites", () => {
     class C extends Nerv.Component {
-      constructor () {
-        super(...arguments)
+      constructor() {
+        super(...arguments);
         this.state = {
           startTime: new Date().getTime(),
           endTime: new Date().getTime() + 2000
-        }
+        };
       }
 
-      tickHandler (time) {
+      tickHandler(time) {}
 
-      }
+      timeupHandler() {}
 
-      timeupHandler () {
-      }
-
-      render () {
-        const { startTime, endTime } = this.state
+      render() {
+        const { startTime, endTime } = this.state;
 
         return (
-          <View className='index'>
-            <Timer startTime={startTime} endTime={endTime} onTick={this.tickHandler} onTimeup={this.timeupHandler} />
+          <View className="index">
+            <Timer
+              startTime={startTime}
+              endTime={endTime}
+              onTick={this.tickHandler}
+              onTimeup={this.timeupHandler}
+            />
           </View>
-        )
+        );
       }
     }
 
-    const TimerRender = sinon.spy(Timer.prototype, 'render')
-    Nerv.render(<C />, scratch)
-    expect(TimerRender.calledOnce).toBeTruthy()
-    expect(scratch.innerHTML).toBe('<div class="index"><div class="timer"><span class="taro-text">0:0:2</span></div></div>')
-  })
-})
+    const TimerRender = sinon.spy(Timer.prototype, "render");
+    Nerv.render(<C />, scratch);
+    expect(TimerRender.calledOnce).toBeTruthy();
+    expect(scratch.innerHTML).toBe(
+      '<div class="index"><div class="timer"><span class="taro-text">0:0:2</span></div></div>'
+    );
+  });
+});
