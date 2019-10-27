0daca700376a921fedf8e936e0f23524aee585ec
chuxu <s17chucc@163.com>
2019-10-27 21:29:23 +0800

    fix #18 ClTabBar 的 active 不能动态设置问题

--- src/components/tabBar/index.tsx
+++ src/components/tabBar/index.tsx
@@ -1,87 +1,78 @@
 import { View } from "@tarojs/components";
-import Taro, { Component } from "@tarojs/taro";
+import Taro, { useState, useEffect } from "@tarojs/taro";
 import { classNames, isNumber } from "../utils";
 import { BG_COLOR_LIST, TEXT_COLOR_LIST } from "../utils/model";
 import { IProps } from "../../../@types/tabBar";
 
-interface IState {
-  activeIndex: number;
-}
+export default function ClTabBar(props: IProps) {
+  const [activeIndex, setActiveIndex] = useState(0);
 
-export default class ClTabBar extends Component<IProps, IState> {
-  static options = {
-    addGlobalClass: true
+  useEffect(() => {
+    setActiveIndex(props.active || 0);
+  }, [props.active]);
+
+  const onClick = (index: number) => {
+    setActiveIndex(index);
+    props.onClick && props.onClick(index);
   };
-  static defaultProps: IProps = {
-    bgColor: "white",
-    activeColor: "blue",
-    active: 0,
-    tabs: []
-  };
-  state: IState = {
-    activeIndex: 0
-  };
-  click(index: number) {
-    this.setState({
-      activeIndex: index
-    });
-    this.props.onClick && this.props.onClick(index);
-  }
-  componentDidMount() {
-    this.setState({
-      activeIndex: this.props.active || 0
-    });
-  }
-  render() {
-    const colorClassName = this.props.bgColor
-      ? BG_COLOR_LIST[this.props.bgColor]
-      : "bg-white";
-    const activeColorClassName = this.props.activeColor
-      ? TEXT_COLOR_LIST[this.props.activeColor]
-      : "text-blue";
-    const barComponent = this.props.tabs.map((item, index) => (
+
+  const colorClassName = props.bgColor
+    ? BG_COLOR_LIST[props.bgColor]
+    : "bg-white";
+  const activeColorClassName = props.activeColor
+    ? TEXT_COLOR_LIST[props.activeColor]
+    : "text-blue";
+  const barComponent = props.tabs.map((item, index) => (
+    <View
+      onClick={() => {
+        onClick(index);
+      }}
+      key={item.icon}
+      className={`action ${item.action ? "add-action" : ""} ${
+        activeIndex === index ? activeColorClassName : ""
+      }`}
+    >
       <View
-        onClick={this.click.bind(this, index)}
-        key={item.icon}
-        className={`action ${item.action ? "add-action" : ""} ${
-          this.state.activeIndex === index ? activeColorClassName : ""
+        className={`${
+          item.img ? "cuIcon-cu-image" : item.icon ? "cuIcon-" + item.icon : ""
         }`}
       >
-        <View
-          className={`${
-            item.img
-              ? "cuIcon-cu-image"
-              : item.icon
-              ? "cuIcon-" + item.icon
-              : ""
-          }`}
-        >
-          {item.badge !== false ? (
-            <View className="cu-tag badge">
-              {isNumber(item.badge) ? item.badge : ""}
-            </View>
-          ) : (
-            ""
-          )}
-        </View>
-        <View>{item.title}</View>
-      </View>
-    ));
-    return (
-      <View
-        className={classNames(
-          `cu-bar tabbar ${colorClassName}`,
-          this.props.className
+        {item.badge !== false ? (
+          <View className="cu-tag badge">
+            {isNumber(item.badge) ? item.badge : ""}
+          </View>
+        ) : (
+          ""
         )}
-        style={Object.assign(
-          this.props.fix
-            ? { position: "fixed", width: "100vw", bottom: "0", zIndex: 10 }
-            : {},
-          this.props.style
-        )}
-      >
-        {barComponent}
       </View>
-    );
-  }
+      <View>{item.title}</View>
+    </View>
+  ));
+  return (
+    <View
+      className={classNames(
+        `cu-bar tabbar ${colorClassName}`,
+        props.className
+      )}
+      style={Object.assign(
+        props.fix
+          ? { position: "fixed", width: "100vw", bottom: "0", zIndex: 10 }
+          : {},
+        props.style
+      )}
+    >
+      {barComponent}
+    </View>
+  );
 }
+
+ClTabBar.options = {
+  addGlobalClass: true
+};
+
+ClTabBar.defaultProps = {
+  bgColor: "white",
+  activeColor: "blue",
+  active: 0,
+  tabs: []
+} as IProps;
